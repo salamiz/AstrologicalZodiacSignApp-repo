0a9a3fb6c77aa4936843909f5a6c9ea864e2a81a
Zulkifli Salami <salamizulkifli23@icloud.com>
2023-03-13 00:29:10 -0400

    Java Source file
    File contains Art class used to generate ASCII art for each zodiac sign on app.

---
+++ Art.java
@@ -0,0 +1,83 @@
+public class Art 
+{
+    static final String ARIES = """        
+        .-.   .-.
+       (_  \\ /  _)
+            |
+            |
+                """;
+    static final String TAURUS = """
+        .     .
+        '.___.'   
+        .'   `.
+       :       :
+       :       :
+        `.___.'
+                """;
+    static final String GEMINI = """
+        ._____.
+          | |    
+          | |
+         _|_|_
+        '     '
+            """;
+    static final String CANCER = """
+         .--.
+        /   _`. 
+       (_) ( )
+      '.    /
+        `--'
+            """;
+    static final String LEO = """
+         .--.
+        (    ) 
+       (_)  /
+           (_,
+            """;
+    static final String VIRGO = """
+         _
+        ' `:--.--.
+           |  |  |_ 
+           |  |  | )
+           |  |  |/
+                (J
+            """;
+    static final String LIBRA = """
+             __
+        ___.'  '.___
+        ____________
+            """;
+    static final String SCORPIO = """
+         _
+        ' `:--.--.
+           |  |  |
+           |  |  |
+           |  |  |  ..,
+                 `---':
+            """;
+    static final String SAGITTARIUS = """
+              ...
+              .': 
+            .'
+        `..'
+        .'`.
+            """;
+    static final String CAPRICORN = """
+                _
+        \\      /_)   
+         \\    /`.
+          \\  /   ;
+           \\/ __.'
+            """;
+    static final String AQUARIUS = """
+        .-"-._.-"-._.-
+        .-"-._.-"-._.-
+            """;
+    static final String PISCES = """
+        `-.    .-' 
+           :  :
+         --:--:--
+           :  :
+        .-'    `-.
+            """;
+}
